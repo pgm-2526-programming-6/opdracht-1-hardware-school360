40e73fadaa757b199e29747729e223a6535380bc
JarneDM <jarne.demeyer05@hotmail.com>
2025-11-25 12:38:45 +0100

    forgot to make branch. no major edits

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,12 +1,12 @@
 import { Link } from "expo-router";
 import { useState } from "react";
 import {
-    Alert,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
 
 export default function LoginPage() {
