4be0c1c1099d9b867c0f6ca7fd05e9802596c48c
ArnoVanSteenbergen03 <arno.van.steenbergen1@gmail.com>
2025-12-09 10:35:56 +0100

    made a final teacher page

--- src/core/modules/attendance/api.attendance.ts
+++ src/core/modules/attendance/api.attendance.ts
@@ -7,8 +7,9 @@ export const getAttendanceSessions = async () => {
     arrival_time,
     departure_time,
     profile_id,
+    campus_id,
     profile:profile_id ( first_name, last_name ),
-    campus:campus_id ( name )
+    campus:campus_id ( id, name )
   `);
   if (response && response.data) {
     return response.data;
